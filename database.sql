f1a2e20e4383148a894a0fca6bedb790ca647ae8
Samuel Solberg <pauliesays65@gmail.com>
2018-10-22 14:09:27 -0500

    workout tabel INSERTs added to sql file

--- database.sql
+++ database.sql
@@ -1,5 +1,23 @@
-CREATE TABLE person (
+CREATE TABLE user (
     id SERIAL PRIMARY KEY,
     username VARCHAR (80) UNIQUE NOT NULL,
     password VARCHAR (1000) NOT NULL
 );
+
+INSERT INTO workout("name")
+VALUES('4x4');
+
+INSERT INTO workout("name", "on_time", "rest_time")
+VALUES('Minimum Edge', 12, 300);
+
+INSERT INTO workout("name", "on_time", "off_time", "rest_time")
+VALUES('7 Second Hangs', 7, 3, 180)
+('10 Second Hangs', 10, 5, 300);
+
+INSERT INTO workout("name", "on_time", "rest_time")
+VALUES('Campus Board', 30, 90);
+
+
+
+
+
