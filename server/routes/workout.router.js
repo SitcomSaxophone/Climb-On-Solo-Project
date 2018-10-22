363b36ba7280e01c58251c434f32b956b24e1ec7
Samuel Solberg <pauliesays65@gmail.com>
2018-10-22 15:33:10 -0500

    server side POST to database up

--- server/routes/workout.router.js
+++ server/routes/workout.router.js
@@ -7,20 +7,38 @@ const router = express.Router();
  */
 router.get('/', (req, res) => {
     pool.query(`SELECT * FROM "workout";`)
-    .then(results => {
-        res.send(results.rows);
-    })
-    .catch(error => {
-        console.log('Error making GET query to database: ', error);
-        res.sendStatus(500);
-    });
+        .then(results => {
+            res.send(results.rows);
+        })
+        .catch(error => {
+            console.log('Error making GET query to database: ', error);
+            res.sendStatus(500);
+        });
 });
 
 /**
  * POST route template
  */
 router.post('/', (req, res) => {
+    let workout = req.body;
+    pool.query(`INSERT INTO "schedule"("start_date", "end_date", "added_weight", "route_rating", "comments", "workout_id", "user_id")
+                VALUES($1, $2, $3, $4, $5, $6, $7);`,
+        [
+            workout.start_date,
+            workout.end_date,
+            workout.added_weight,
+            workout.route_rating,
+            workout.comments,
+            workout.workout_id,
+            workout.user_id
+        ])
+    .then(() => {
 
+    })
+    .catch(error => {
+        console.log('Error making POST to database: ', error);
+        res.sendStatus(500);
+    });
 });
 
 module.exports = router;
