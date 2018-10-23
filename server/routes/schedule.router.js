ba46be9d3ed9b4083c970e5e0933de21a56e32b8
Samuel Solberg <pauliesays65@gmail.com>
2018-10-23 08:02:46 -0500

    server side GET route to schedule table up

---
+++ server/routes/schedule.router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
+
+router.get('/', (req, res) => {
+    pool.query(`SELECT * FROM "schedule";`)
+    .then(results => {
+        res.send(results.rows);
+    })
+    .catch(error => {
+        console.log('Error making GET to database: ', error);
+        res.sendStatus(500);
+    });
+})
+
+module.exports = router;
