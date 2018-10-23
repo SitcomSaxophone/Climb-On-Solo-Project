b40fb236a854e628cb4d60ad2908e044e58f86c8
Samuel Solberg <pauliesays65@gmail.com>
2018-10-23 15:39:28 -0500

    updated new scheduled workout form with onChange events

--- server/routes/schedule.router.js
+++ server/routes/schedule.router.js
@@ -12,6 +12,29 @@ router.get('/', (req, res) => {
         console.log('Error making GET to database: ', error);
         res.sendStatus(500);
     });
-})
+});
+
+router.post('/', (req, res) => {
+    let workout = req.body;
+    pool.query(`INSERT INTO "schedule"("start_date", "end_date", "added_weight", "route_rating", "comments", "workout_id", "user_id")
+                VALUES($1, $2, $3, $4, $5, $6, $7);`,
+        [
+            workout.start_date,
+            workout.end_date,
+            workout.added_weight,
+            workout.route_rating,
+            workout.comments,
+            workout.workout_id,
+            workout.user_id
+        ])
+    .then(() => {
+        res.sendStatus(201);
+    })
+    .catch(error => {
+        console.log('Error making POST to database: ', error);
+        res.sendStatus(500);
+    });
+});
+
 
 module.exports = router;
--- src/components/WorkoutList/WorkoutList.js
+++ src/components/WorkoutList/WorkoutList.js
@@ -12,7 +12,7 @@ class WorkoutList extends Component {
   state = {
     newWorkout: {
       workout_id: 0,
-      user_id: 0,
+      user_id: this.props.user.id,
       added_weight: 0,
       route_rating: '',
       comments: '',
@@ -25,6 +25,38 @@ class WorkoutList extends Component {
     this.props.dispatch({type: 'FETCH_WORKOUT'});
   }
 
+  scheduleNewWorkout = params => event => {
+    event.preventDefault();
+    // this.props.dispatch({type: 'SCHEDULE_WORKOUT', payload: this.state.newWorkout});
+    axios({
+      method: 'POST',
+      url: '/api/schedule',
+      data: this.state.newWorkout,
+    }).then(() => {
+      this.props.dispatch({type: 'FETCH_WORKOUT'});
+    }).catch(error => {
+      alert('Error making POST to server: ', error);
+    });
+  }
+
+  handleChangeFor = property => event => {
+    this.setState({
+      newWorkout:{
+        ...this.state.newWorkout,
+        [property]: event.target.value,
+      }
+    });
+  }
+
+  handleSelectChange = event => {
+    this.setState({
+      newWorkout: {
+        ...this.state.newWorkout,
+        workout_id: Number([...event.target.selectedOptions].filter(o => o.selected).map(o => o.value)),
+      }
+    })
+  }
+
   render() {
     return (
       <div>
@@ -32,22 +64,51 @@ class WorkoutList extends Component {
           Workout List
     </h2>
 
-        <form>
-          <input type="time" placeholder="Start Time" />
-          <input type="time" placeholder="End Time" />
-          <input type="number" placeholder="Workout ID" />
-          <input type="number" placeholder="Additional weight (optional)" />
-          <input type="text" placeholder="Route Rating (optional)" />
-          <textarea placeholder="Any additional comments?"/>
+        <form onSubmit={this.scheduleNewWorkout()}>
+          <input 
+            type="time" 
+            placeholder="Start Time" 
+            onChange={this.handleChangeFor('start_time')}
+          />
+          <input 
+            type="time" 
+            placeholder="End Time" 
+            onChange={this.handleChangeFor('end_time')}
+          />
+          <select onChange={this.handleSelectChange}>
+            <option value="">--Choose a workout--</option>
+            {this.props.workouts.map(workout => (
+            <option 
+              key={workout.id} 
+              value={workout.id}
+            >
+              {workout.name}
+            </option>))}
+          </select>
+          <input 
+            type="number" 
+            placeholder="Additional weight (optional)" 
+            onChange={this.handleChangeFor('added_weight')}
+          />
+          <input 
+            type="text" 
+            placeholder="Route Rating (optional)" 
+            onChange={this.handleChangeFor('route_rating')}
+          />
+          <textarea 
+            placeholder="Any additional comments?"
+            onChange={this.handleChangeFor('comments')}
+          />
           <input type="submit" />
         </form>
-        {JSON.stringify(this.props.workouts, null, 2)}
+        {JSON.stringify(this.state, null, 2)}
       </div>
     )
   }
 }
 
 const mapStateToProps = state => ({
+  user: state.user,
   workouts: state.workout,
 })
 
