b50e6a9662ec76b39c308741d2da3bd55bcf59e2
Samuel Solberg <pauliesays65@gmail.com>
2018-11-06 12:00:38 -0600

    run build

---
+++ build/precache-manifest.ce91832b2ba1ad73461d3b28e17133e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9d1869f80925603dcde0",
+    "url": "/static/js/main.9d1869f8.chunk.js"
+  },
+  {
+    "revision": "8ef90d84d0e3e6131004",
+    "url": "/static/js/1.8ef90d84.chunk.js"
+  },
+  {
+    "revision": "9d1869f80925603dcde0",
+    "url": "/static/css/main.0157034e.chunk.css"
+  },
+  {
+    "revision": "f84a4fa8e5120b0488e484d1579bf895",
+    "url": "/index.html"
+  }
+];
